a0a60660f289295445f791818b2dd51a0482ad13
Guilherme Vieira <guilhermesv@gmail.com>
2025-06-25 19:44:32 -0300

    Teste de amplitude controlando saturação.

--- codigo-openlab/Instalacao/amplitude.ino
+++ codigo-openlab/Instalacao/amplitude.ino
@@ -1,6 +1,6 @@
 // Ajustar a escala para todas as esteiras, 
 // talvez seja legal uma escala de ruido grande
-
+// Testei a amplitude controlando a saturacao, verificar
 
 void efeito_amplitude() {
   int peakToPeak = getAmplitude(); // Calcula a amplitude do sinal
@@ -11,6 +11,7 @@ void efeito_amplitude() {
   }
   int brilho_max = map(sensor, 0, 3000, 0, 255);
   int hue = 255-mod8(t_2 + 127, 255);
+  int sat = map(sensor, 0, 3000, 0, 255);
 
   EVERY_N_MILLISECONDS(20) {
     fadeToBlackBy(leds, NUM_LEDS, 20);
@@ -23,14 +24,15 @@ void efeito_amplitude() {
   // int scale = 10;
   // uint16_t t = millis() / 10;
 
-  int scale = 1;
+  int scale = 4;
   uint16_t t = millis() / 10;
 
   for (int i = 0; i < NUM_LEDS; i++) {
     uint8_t noise = inoise8(i * scale + x + t);
     if ( noise > A_NOISE_LIMIAR) {
       // leds[i] = CHSV(hue, 255, brilho_max);
-      leds[i] = CHSV(hue, 255, avg8(noise, brilho_max));
+      // leds[i] = CHSV(hue, 255, avg8(noise, brilho_max));
+      leds[i] = CHSV(hue, sat, avg8(noise, brilho_max));
       // leds[i] = CHSV(hue, 255, qsub8(brilho_max, noise));
     }
   }
