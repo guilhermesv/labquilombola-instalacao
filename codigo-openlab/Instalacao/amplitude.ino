58f5ba6e62c69a97a2d5fe69f179230de8bfda12
Guilherme Vieira <guilhermesv@gmail.com>
2025-06-28 18:31:29 -0300

    Inverti a satarução

--- codigo-openlab/Instalacao/amplitude.ino
+++ codigo-openlab/Instalacao/amplitude.ino
@@ -11,7 +11,7 @@ void efeito_amplitude() {
   }
   int brilho_max = map(sensor, 0, 3000, 0, 255);
   int hue = 255-mod8(t_2 + 127, 255);
-  int sat = map(sensor, 0, 3000, 0, 255);
+  int sat = map(sensor, 0, 3000, 255, 0);
 
   EVERY_N_MILLISECONDS(20) {
     fadeToBlackBy(leds, NUM_LEDS, 20);
